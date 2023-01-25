6a007db61a0162d63607617aedb51d16b1ec81fc
BinhodevTrybe <binhuhue@gmail.com>
2023-01-25 13:27:54 -0300

    exercize-bubble-sort

--- fundamentos/secao3-introducaoJavaScript/dia2-Array-e-loop-for/script.js
+++ fundamentos/secao3-introducaoJavaScript/dia2-Array-e-loop-for/script.js
@@ -79,4 +79,17 @@ let lowestNumber = numbers[0];
      let count = numbers[index] / 2;
      console.log(count)
      }
-      */
+      */ 
+      l/* et numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let index = 1; index < numbers.length; index += 1) {
+  for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+    if (numbers[index] < numbers[secondIndex]) {
+      let position = numbers[index];
+      numbers[index] = numbers[secondIndex];
+      numbers[secondIndex] = position;
+    }
+  }
+}
+
+console.log(numbers); */
