81295aa3dd133c606160bcd87d8d406f8885efaf
BinhodevTrybe <binhuhue@gmail.com>
2023-01-26 18:57:35 -0300

    exercicios

---
+++ fundamentos/secao3-introducaoJavaScript/dia4-Objects-and-Functions/para-fixar/script.js
@@ -0,0 +1,68 @@
+/* FIXANDO OBJECTS
+ let name = 'Marta';
+let lastName = 'Silva';
+let age = 34;
+let medals = { golden: 2, silver: 3 };
+ */
+/* let player = {
+name: "Marta",
+lastName: 'Silva',
+age: 34,
+medals: {
+    golden: 2,
+    silver: 3
+},
+};
+console.log(player.bestInTheWorld =[2006, 2007, 2008, 2009, 2010, 2018]);
+console.log('A jogadora ' + player['name'] + ' ' + player['lastName'] + ' foi eleita a melhor do mundo por ' + player['bestInTheWorld'].length + ' vezes');
+console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata' ); 
+
+FIXANDO - FOR OF E FOR IN*/
+
+/* let names = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge',
+  };
+
+  for (const key in names) {
+    console.log('Olá ' + names[key]);
+    } */
+/*   let car = {
+      model: 'A3 Sedan',
+      manufacturer: 'Audi',
+      year: 2020
+    };
+
+  for (const key in car) {
+    console.log(key, car[key])
+      } 
+      
+      FIXANDO - FUNCTIONS*/
+
+      /* function soma(a, b) {
+        return a + b;
+      }
+      console.log(soma(11, 22)); */
+
+      /* function subtracao(a, b) {
+        return a - b;
+      }
+      console.log(subtracao(40, 50)); */
+
+     /*  function multiplicacao(a, b) {
+        return a * b;
+      }
+      console.log(multiplicacao(40, 50)); */
+
+     /*  function divisao(a, b) {
+        return a / b;
+      }
+      console.log(divisao(40, 50)); */
+
+     /*  function modulo(a, b) {
+        return a % b;
+      }
+      console.log(modulo(40, 50)); */
+
+      
